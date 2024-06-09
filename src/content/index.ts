953fa1daec1442f5715a24d397b82ae2625801cd
Wataru Ono <wtr0414@gmail.com>
2024-06-09 14:39:19 +0900

    refactor: restaurantの方が適切であるため

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,23 +1,23 @@
 const main = () => {
-  const storeListElement = document.querySelectorAll(".list-rst__rst-name a");
+  const restaurantListElement = document.querySelectorAll(".list-rst__rst-name a");
 
   // 初期段階では食べログのみ対応する
   const isTabelog = window.location.hostname === "tabelog.com";
   if (!isTabelog) return;
 
-  if (storeListElement === null) {
+  if (restaurantListElement === null) {
     console.log("店舗一覧を表す要素が見つかりません");
   }
 
-  for (const storeElement of storeListElement) {
-    if (storeElement.textContent === null) {
+  for (const restaurantElement of restaurantListElement) {
+    if (restaurantElement.textContent === null) {
       console.error("店舗を表す要素が見つかりません");
       break;
     }
 
-    if (!storeElement.textContent.includes("個室")) continue;
+    if (!restaurantElement.textContent.includes("個室")) continue;
 
-    const parentElement = storeElement.closest(".list-rst");
+    const parentElement = restaurantElement.closest(".list-rst");
     if (parentElement === null) {
       console.error("親要素が見つかりません");
       break;
