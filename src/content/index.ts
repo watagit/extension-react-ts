534d166b0fe55a80f15fddb2ad19089a8c6e14f5
Wataru Ono <wtr0414@gmail.com>
2024-06-09 14:38:24 +0900

    feat: 食べログのみで動作させる

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,24 +1,32 @@
-const storeListElement = document.querySelectorAll(".list-rst__rst-name a");
+const main = () => {
+  const storeListElement = document.querySelectorAll(".list-rst__rst-name a");
 
-if (storeListElement === null) {
-  console.log("店舗一覧を表す要素が見つかりません");
-}
+  // 初期段階では食べログのみ対応する
+  const isTabelog = window.location.hostname === "tabelog.com";
+  if (!isTabelog) return;
 
-for (const storeElement of storeListElement) {
-  if (storeElement.textContent === null) {
-    console.error("店舗を表す要素が見つかりません");
-    break;
+  if (storeListElement === null) {
+    console.log("店舗一覧を表す要素が見つかりません");
   }
 
-  if (!storeElement.textContent.includes("個室")) continue;
+  for (const storeElement of storeListElement) {
+    if (storeElement.textContent === null) {
+      console.error("店舗を表す要素が見つかりません");
+      break;
+    }
 
-  const parentElement = storeElement.closest(".list-rst");
-  if (parentElement === null) {
-    console.error("親要素が見つかりません");
-    break;
+    if (!storeElement.textContent.includes("個室")) continue;
+
+    const parentElement = storeElement.closest(".list-rst");
+    if (parentElement === null) {
+      console.error("親要素が見つかりません");
+      break;
+    }
+
+    // closest()からの返り値は型定義上Elementだが、
+    // 実際はHTMLElementなのでキャストを許容する
+    (parentElement as HTMLElement).style.display = "none";
   }
+};
 
-  // closest()からの返り値は型定義上Elementだが、
-  // 実際はHTMLElementなのでキャストを許容する
-  (parentElement as HTMLElement).style.display = "none";
-}
+main();
