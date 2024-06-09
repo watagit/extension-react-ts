d341fae271a9ad3a0dc1076c133a4391450a28d1
Wataru Ono <wtr0414@gmail.com>
2024-06-09 13:38:50 +0900

    refactor: 不要なライブラリを削除する

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,14 +1,7 @@
-import { Counter } from "../app/features/counter";
-
 const Popup = () => {
   document.body.className = "w-[30rem] h-[15rem]";
 
-  return (
-    <>
-      <div className="flex justify-center mt-2 text-base">Popup Counter</div>
-      <Counter />
-    </>
-  );
+  return <h1>Safe Dining Filter</h1>;
 };
 
 export default Popup;
