a3194eb8c886b86a4b5b1f6493c354f39021f84b
Wataru Ono <wtr0414@gmail.com>
2024-06-09 17:27:48 +0900

    refactor: chromeのstorageに保存する
    tmp

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,45 +1,63 @@
 import { Checkbox, VStack } from "@chakra-ui/react";
-import { useState } from "react";
-import { useKeywords } from "../store/useKeyword";
+import { getBucket } from "@extend-chrome/storage";
+import { useEffect, useState } from "react";
 
 document.body.className = "w-[20rem] h-[15rem]";
 
+type Bucket = {
+  keywords: string[];
+};
+
+const bucket = getBucket<Bucket>("my_bucket", "local");
+
 const Popup = () => {
-  const [checkedKeywords, setCheckedKeywords] = useState<boolean[]>([
-    true,
-    false,
-  ]);
-  const { keywords, addKeyword, removeKeyword } = useKeywords();
+  const [keywords, setKeywords] = useState<string[]>([]);
+
+  useEffect(() => {
+    async () => {
+      const value = await bucket.get();
+      if (value.keywords) {
+        setKeywords(value.keywords);
+      }
+    };
+  }, []);
+
+  const addKeyword = (keyword: string) => {
+    const newKeywords = [...keywords, keyword];
+    bucket.set({ keywords: newKeywords });
+    setKeywords(newKeywords);
+  };
+
+  const removeKeyword = (keyword: string) => {
+    const newKeywords = keywords.filter((k) => k !== keyword);
+    bucket.set({ keywords: newKeywords });
+    setKeywords(newKeywords);
+  };
 
   return (
     <VStack align="flex-start">
       <p>{String(keywords)}</p>
       <Checkbox
-        isChecked={checkedKeywords[0]}
-        onChange={
-          (event) => {
-            if (event.target.checked) {
-              addKeyword("個室");
-            } else {
-              removeKeyword("個室");
-            }
-            setCheckedKeywords([event.target.checked, checkedKeywords[1]])
+        isChecked={keywords.includes("個室")}
+        onChange={(event) => {
+          if (event.target.checked) {
+            addKeyword("個室");
+          } else {
+            removeKeyword("個室");
           }
-        }
+        }}
       >
         個室
       </Checkbox>
       <Checkbox
-        isChecked={checkedKeywords[1]}
+        isChecked={keywords.includes("創作")}
         onChange={(event) => {
-            if (event.target.checked) {
-              addKeyword("創作");
-            } else {
-              removeKeyword("創作");
-            }
-          setCheckedKeywords([checkedKeywords[0], event.target.checked])
-        }
-        }
+          if (event.target.checked) {
+            addKeyword("創作");
+          } else {
+            removeKeyword("創作");
+          }
+        }}
       >
         創作
       </Checkbox>
