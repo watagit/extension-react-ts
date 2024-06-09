2e6b6e86f391d4ba9378e818331463e5fca62b28
Wataru Ono <wtr0414@gmail.com>
2024-06-09 13:55:25 +0900

    feat: contentを移行する

--- src/content/Content.tsx
+++
@@ -1,9 +0,0 @@
-const Content = () => {
-  return (
-    <div className="fixed z-[999] bottom-2 right-2 shadow-xl border-[1px] bg-white bg-opacity-10">
-      <div className="flex justify-center mt-2 text-base">Content Counter</div>
-    </div>
-  );
-};
-
-export default Content;
